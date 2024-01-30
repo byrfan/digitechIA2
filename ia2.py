f66101097d0fc62e122e8d96b8938a23c66c48ea
Byron Fanti <bfant@mybce.catholic.edu.au>
2024-01-30 13:42:34 +1000

    added trucks to index

--- ia2.py
+++ ia2.py
@@ -12,6 +12,6 @@ else:
 
 @app.route("/")
 def landing():
-    return render_template("index.html")
+    return render_template("index.html", db.truck_example(8))
 
 app.run(host="127.0.0.1", port="5000", debug=True)
